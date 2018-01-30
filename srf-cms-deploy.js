c39d2c00f8ae1e48fdc969a28bec4c1c5b8f0f62
Philipp Christen <philipp.christen@srf.ch>
2018-01-30 13:17:12 +0100

    simple prev/next buttons

--- srf-cms-deploy.js
+++ srf-cms-deploy.js
@@ -9,13 +9,102 @@
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
-(function() {
-    $(function() {
+(() => {
+    $(() => {
+        const ACTIVE_CLASS = "srf-interesting-span--marked";
 
-        var button = $('<button>Help me!</button>');
+        let button = $('<button>Help me!</button>');
         $('.build-caption.page-headline').append(button);
-        button.on('click', function(){
-            var consoleContent = $('.console-output').html();
-        })
+
+        button.on('click', () => {
+            let consoleContent = $('.console-output').html();
+
+            collectInterestingSpans();
+            createJumpButtons();
+        });
+
+        let redSpans = $('span[style*="color: #CD0000"]');
+
+        let createJumpButtons = () => {
+            let $prevBtn = $('<button class="srf-btn srf-btn--prev">Previous</button>');
+            let $nextBtn = $('<button class="srf-btn srf-btn--next">Next</button>');
+
+            $prevBtn.on("click", () => {
+                let $markedSpan = $(`.${ACTIVE_CLASS}`);
+
+                if ($markedSpan.length === 0) {
+                    markInterestingSpan($(".srf-interesting-span").last());
+                } else {
+                    let $prevElem = $markedSpan.prev(".srf-interesting-span");
+                    if ($prevElem) {
+                        markInterestingSpan($prevElem);
+                        $markedSpan.removeClass(ACTIVE_CLASS);
+                    }
+                }
+            });
+
+            $nextBtn.on("click", () => {
+                let $markedSpan = $(`.${ACTIVE_CLASS}`);
+
+                if ($markedSpan.length === 0) {
+                    markInterestingSpan($(".srf-interesting-span").first());
+                } else {
+                    let $nextElem = $markedSpan.next(".srf-interesting-span");
+                    if ($nextElem) {
+                        markInterestingSpan($nextElem);
+                        $markedSpan.removeClass(ACTIVE_CLASS);
+                    }
+                }
+            });
+
+            $('body').append($prevBtn);
+            $('body').append($nextBtn);
+        };
+
+        let collectInterestingSpans = () => {
+            $('span[style*="color: #CD0000"]').addClass("srf-interesting-span");
+        };
+
+        let markInterestingSpan = ($span) => {
+            $span.addClass(ACTIVE_CLASS);
+            $span.focus();
+
+            $('html, body').animate({
+                scrollTop: $span.offset().top
+            }, 1000);
+        };
+
+
+
+
+
+        let addCss = (cssString) => {
+            var head = document.getElementsByTagName('head')[0];
+            if(!head) return;
+
+            var newCss = document.createElement('style');
+            newCss.type = "text/css";
+            newCss.innerHTML = cssString;
+            head.appendChild(newCss);
+        };
+
+        addCss (
+            `
+            .srf-interesting-span--marked {
+                border: 1px solid red;
+                padding: 3px;
+            }
+            .srf-btn {
+                position: fixed;
+                top: 40px;
+                right: 100px;
+                padding: 8px 16px;
+            }
+            
+            .srf-btn--prev {
+                right: 180px;
+            }
+            `
+        );
     });
 })();
